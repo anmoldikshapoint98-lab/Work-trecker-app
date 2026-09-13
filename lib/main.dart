e2e77821be8969a0472f2e15140f8b0e1494d667
anmoldikshapoint98-lab <anmoldikshapoint98@gmail.com>
2026-09-13 15:55:11 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WorkTrackerApp());
+}
+
+class WorkTrackerApp extends StatelessWidget {
+  const WorkTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WORK TRACKER',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WORK TRACKER'),
+      ),
+      body: const Center(
+        child: Text(
+          'WORK TRACKER',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
